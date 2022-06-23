3824b8a36c86ebae1587fb75263ac0d1f797010e
Marcia Agostinho <marciaagostinho27@gmail.com>
2022-06-23 13:40:09 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,18 @@
+### Desafio Dev Quest JavaScript Básico 🚀🚀
+
+
+### Segue imagem do código.
+
+[<img src="./js-basico.png">]
+
+
+
+###  Criando mais de uma função.
+### Com os conhecimentos adquiridos na aula de
+### funções crie uma função para cada operação
+### matemática (soma, divisão, multiplicação e subtração).
+
+### Realize a chamada de cada função, colocando
+### cada resultado em uma variável e mostre a resposta usando o console.log.
+
+
---
+++ index.js
@@ -0,0 +1,42 @@
+
+function soma(number1, number2) {
+    let soma = number1 + number2;
+
+    return "A soma dos dois números é:" + ' ' + soma
+
+}
+
+let resultadodaSoma = soma(5, 5)
+console.log(resultadodaSoma)
+
+function diminuir(number3, number4) {
+    let diminuir = number3 - number4;
+    return "A subtração dos dois números é:" + ' ' + diminuir
+
+
+
+}
+
+let resultadodaSubtracao = diminuir(6, 6)
+console.log(resultadodaSubtracao)
+
+
+function multiplica(number5, number6) {
+    let multiplica = number5 * number6;
+    return "A multiplicação dos dois números é:" + ' ' + multiplica
+
+}
+
+let resultadoDamultiplicacao = multiplica(4, 8)
+
+console.log(resultadoDamultiplicacao)
+
+function divisao(number7, number8) {
+    let divisao = number7 / number8;
+    return "A divisão dos dois números é:" + ' ' + divisao
+
+}
+
+let resultadoDaDivisao = divisao(12, 6)
+
+console.log(resultadoDaDivisao)
